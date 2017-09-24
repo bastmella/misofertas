818546b447741017fab7f4211ad0233d76b284ae
bastmella <bastmella@users.noreply.github.com>
2017-09-24 19:16:09 -0300

    avance

--- MisOfertasDesktop/MisOfertasDesktop/Login.cs
+++ MisOfertasDesktop/MisOfertasDesktop/Login.cs
@@ -20,16 +20,45 @@ namespace MisOfertasDesktop
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private OracleConnection Conectar()
         {
-            string oradb = ConfigurationManager.ConnectionStrings["MisOfertasDesktop.Properties.Settings.ConnectionString"].ConnectionString;
+            string CadenaConexion = ConfigurationManager.ConnectionStrings["MisOfertasDesktop.Properties.Settings.ConnectionString"].ConnectionString;
+            CadenaConexion = string.Format(CadenaConexion, "BDMISOFERTAS", "bsam123");
             OracleConnection conn = new OracleConnection();
-            conn.ConnectionString = oradb;
+            conn.ConnectionString = CadenaConexion;
             try
             {
                 conn.Open();
-                MessageBox.Show(conn.State.ToString());
-            }catch (Exception ex) { MessageBox.Show(ex.Message); }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return conn;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = Conectar();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "valid_user";
+            cmd.BindByName = true;
+
+            string user_t = Convert.ToString(txt_user.Text.Trim());
+            string pass_t = Convert.ToString(txt_pass.Text.Trim());
+
+            cmd.Parameters.Add(new OracleParameter("p_correo", OracleDbType.Varchar2)).Value = user_t;
+            cmd.Parameters.Add(new OracleParameter("p_password", OracleDbType.Varchar2)).Value = pass_t;
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                if (cmd.Parameters.)
+                {
+
+                }
+            }
             /*MenuPrincipal menu = new MenuPrincipal();
             menu.Show();
             this.Hide();*/
